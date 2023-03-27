bd6262d25e055062f73d50818c9c8c5047fb7b77
Ruther John Madelo <madelorutherjohn12@gmail.com>
2023-03-27 11:14:17 +0800

    added firebase

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'Screen/ListScreen.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Assignment1());
 }
 
