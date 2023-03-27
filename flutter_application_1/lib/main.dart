ee1383a951af8d57253e505e0756b8950b062e53
Ruther John Madelo <madelorutherjohn12@gmail.com>
2023-03-27 10:38:37 +0800

    added lab

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LabWorkMain());
+}
+
+class LabWorkMain extends StatelessWidget {
+  const LabWorkMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
